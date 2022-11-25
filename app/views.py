d7dccc65c94a2f59a72fbc5bcebe753fe16cf760
Pedro Avelar <pedro-curiel@hotmail.com>
2022-11-25 14:38:51 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -23,4 +23,18 @@ def view(request, pk):
   data['db'] = Livros.objects.get(pk=pk)
   return render(request, 'view.html', data)
 
+def edit(request, pk):
+  data = {}
+  data['db'] = Livros.objects.get(pk=pk)
+  data['form'] = LivrosForm(instance=data['db'])
+  return render(request, 'form.html', data)
+
+def update(request, pk):
+  data = {}
+  data['db'] = Livros.objects.get(pk=pk)
+  form = LivrosForm(request.POST or None, instance=data['db'])
+  if form.is_valid():
+    form.save()
+    return redirect('home')
+
   
