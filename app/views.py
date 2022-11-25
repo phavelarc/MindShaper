6505920dc5835e03d7447836099f56c3c8878e4c
Pedro Avelar <pedro-curiel@hotmail.com>
2022-11-25 14:10:57 -0300

    update views

--- app/views.py
+++ app/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.forms import LivrosForm
+from app.models import Livros
 # Create your views here.
 def home(request):
-  return render(request, 'index.html')
+  data = {}
+  data['db'] = Livros.objects.all()
+  return render(request, 'index.html', data)
 
 def form(request):
   data = {}
   data['form'] = LivrosForm()
   return render(request, 'form.html', data)
+
+def create(request):
+  form = LivrosForm(request.POST or None)
+  if form.is_valid():
+    form.save()
+    return redirect('home')
+
+def view(request, pk):
+  data = {}
+  data['db'] = Livros.objects.get(pk=pk)
+  return render(request, 'view.html', data)
+
   
